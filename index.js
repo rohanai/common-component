95bc29f4c279f08a1f74eb41cc357f51d6166049
rohan <rohanambhore01@gmail.com>
2020-05-26 20:22:31 +0530

    frst

---
+++ index.js
@@ -0,0 +1,9 @@
+export { default as Button } from "./src/components/Buttons";
+export { default as PrimaryButton } from "./src/components/Buttons";
+export { default as SecondaryButton } from "./src/components/Buttons";
+export { default as TertiaryButton } from "./src/components/Buttons";
+
+export { default as SignUpModal } from "./src/components/Modals";
+export { default as SignInModal } from "./src/components/Modals";
+
+export { default as utils } from "./src/utils";
